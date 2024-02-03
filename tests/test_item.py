025a15f7e5f9031561b3d7b4246ff7e160e4ea2b
Pavel <drago_new@bk.ru>
2024-02-03 18:12:12 +0300

    fix test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -43,3 +43,9 @@ def test_string_to_number():
     """Проверка возвращения числа из строки-числа"""
     assert Item.string_to_number("5") == 5
     assert Item.string_to_number("1.5") == 1
+
+
+def test_repr_and_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert str(item1) == 'Смартфон'
