028a6f241cdf9979438207591cccc630bc2e5ae9
Pavel <drago_new@bk.ru>
2024-02-03 18:11:57 +0300

    fix item.py

--- src/item.py
+++ src/item.py
@@ -1,5 +1,6 @@
 import csv
 
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -19,6 +20,12 @@ class Item:
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
 
     def calculate_total_price(self) -> float:
         """
